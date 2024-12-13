68d1ddda48339edd4e675d4553a25f471a167228
nikito <nicolas.melia@mi.unc.edu.ar>
2024-12-12 22:31:59 -0300

    new pics added

--- js/team-members.js
+++ js/team-members.js
@@ -1,17 +1,31 @@
 const teamMembers = [
-    {
-        name: "Nicolás Melia",
-        nick: "nico",
-        role: "Profe",
-        img: "../assets/img/niko2.png",
-        bio: "Hola soy Nico!, profe de CodAr"
-    },
     { 
         name: "Matilde Tolosa",
         nick: "matu",
         role: "Fundadora",
         img: "../assets/img/matu.png",
-        bio: "---"
+        bio: "Fundadora, Directora Ejecutiva y Profesora"
+    },
+    { 
+        name: "Alejo Vazquez",
+        nick: "ale",
+        role: "Fundador y Asesor Educativo",
+        img: "../assets/img/alejo.png",
+        bio: "Alejo Vazquez, fundador y asesor educativo"
+    },
+    { 
+        name: "Erika González",
+        nick: "Erika",
+        role: "Asesora Educativa",
+        img: "../assets/img/eri.png",
+        bio: "Asesora Educativa"
+    },
+    {
+        name: "Nicolás Melia",
+        nick: "nico",
+        role: "Profe",
+        img: "../assets/img/niko2.png",
+        bio: "Profe y Web Developer"
     }
   ];
   
